b9b567eeb408d48ca544804a61587c689191664e
phildong <35715936+phildong@users.noreply.github.com>
2024-04-12 17:22:33 -0400

    implemented two pass registration algorithm

--- 00.co-registration.py
+++ 00.co-registration.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import xarray as xr
 
-from routine.coregistration import apply_tx, est_sim, process_temp
+from routine.coregistration import apply_tx, estimate_tranform, process_temp
 from routine.io import read_templates
 from routine.plotting import plot_ims
 
@@ -13,13 +13,13 @@ DS = {
         "ms": "./data/demo/21271R/Inscopix_flip_flip.tif",
         "conf": "./data/demo/21271R/ZEISS_405.tif",
         "flip": False,
-        "scal_init": 1 / 1.9,
+        "scal_init": 1.9,
     },
     "21272R": {
         "ms": "./data/demo/21272R/Inscopix_Fip_fip.tif",
         "conf": "./data/demo/21272R/ZEISS_405.tif",
         "flip": False,
-        "scal_init": 1 / 1.9,
+        "scal_init": 1.9,
     },
     "25607": {
         "ms": "./data/demo/25607/Insccopix.tif",
@@ -28,7 +28,6 @@ DS = {
         "scal_init": 1,
     },
 }
-PARAM_VERBOSE = True
 FIG_PATH = "./figs/co-registration"
 
 os.makedirs(FIG_PATH, exist_ok=True)
@@ -51,17 +50,17 @@ for dsname, dsdat in DS.items():
         output_core_dims=[["height", "width"]],
         kwargs={"back_wnd": (151, 151)},
     ).rename("conf-ps")
-    tx, param_df = est_sim(
+    tx, tx_exh, param_df = estimate_tranform(
         im_ms_ps.data,
         im_conf_ps.data,
         scal_init=dsdat["scal_init"],
-        scal_stp=5e-3,
+        scal_stp=5e-2,
         scal_nstp=2,
-        trans_stp=(3, 3),
-        trans_nstp=(15, 15),
-        ang_stp=np.deg2rad(3),
-        ang_nstp=5,
-        verbose=PARAM_VERBOSE,
+        trans_stp=(5, 5),
+        trans_nstp=(12, 12),
+        ang_stp=np.deg2rad(5),
+        ang_nstp=3,
+        lr=1e-3,
     )
     im_ms_reg = xr.DataArray(
         apply_tx(im_ms, tx, ref=im_conf.data),
@@ -77,10 +76,14 @@ for dsname, dsdat in DS.items():
     )
     ps_diff = (im_conf_ps - ps_ms_reg).rename("ps-diff")
     im_diff = (im_conf - im_ms_reg).rename("diff")
-
     fig = plot_ims(
         [im_ms, im_ms_ps, im_conf, im_conf_ps, im_ms_reg, ps_diff, im_diff],
         facet_col_wrap=4,
         norm=True,
     )
     fig.write_html(os.path.join(FIG_PATH, "{}.html".format(dsname)))
+    print(
+        "data: {}, scale: {}, angle: {}, shift: {}".format(
+            dsname, 1 / tx.GetScale(), np.rad2deg(tx.GetAngle()), tx.GetTranslation()
+        )
+    )
--- routine/coregistration.py
+++ routine/coregistration.py
@@ -42,18 +42,19 @@ def it_callback(reg, param_dict):
 def est_sim(
     src: np.ndarray,
     dst: np.ndarray,
+    exhaustive: bool,
+    trans_init=None,
     src_ma=None,
     dst_ma=None,
     lr: float = 0.5,
     niter: int = 1000,
-    scal_init=None,
+    scal_init=1.9,
     scal_stp=1e-2,
     scal_nstp=3,
     ang_stp=np.deg2rad(1),
     ang_nstp=3,
     trans_stp=(1.0, 1.0),
     trans_nstp=(3, 3),
-    verbose=False,
 ):
     src = sitk.GetImageFromArray(src.astype(np.float32))
     dst = sitk.GetImageFromArray(dst.astype(np.float32))
@@ -62,19 +63,14 @@ def est_sim(
         reg.SetMetricMovingMask(sitk.GetImageFromArray(src_ma.astype(np.uint8)))
     if dst_ma is not None:
         reg.SetMetricFixedMask(sitk.GetImageFromArray(dst_ma.astype(np.uint8)))
-    if scal_init is None:
-        scal_init = 1 / 2.5
-    trans_opt = sitk.CenteredTransformInitializer(
-        dst,
-        src,
-        sitk.Similarity2DTransform(scal_init),
-        sitk.CenteredTransformInitializerFilter.GEOMETRY,
-    )
-    # reg.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
-    # reg.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
-    reg.SetInitialTransform(trans_opt)
-    # reg.SetMetricAsMeanSquares()
-    # reg.SetMetricAsANTSNeighborhoodCorrelation(2)
+    if trans_init is None:
+        trans_init = sitk.CenteredTransformInitializer(
+            dst,
+            src,
+            sitk.Similarity2DTransform(1 / scal_init),
+            sitk.CenteredTransformInitializerFilter.GEOMETRY,
+        )
+    reg.SetInitialTransform(trans_init)
     reg.SetMetricAsCorrelation()
     reg.SetInterpolator(sitk.sitkLinear)
     # reg.SetOptimizerAsRegularStepGradientDescent(
@@ -82,8 +78,20 @@ def est_sim(
     #     minStep=1e-7,
     #     numberOfIterations=niter,
     # )
-    reg.SetOptimizerAsExhaustive([scal_nstp, ang_nstp, trans_nstp[0], trans_nstp[1]])
-    reg.SetOptimizerScales([scal_stp, ang_stp, trans_stp[0], trans_stp[1]])
+    if exhaustive:
+        reg.SetOptimizerAsExhaustive(
+            [scal_nstp, ang_nstp, trans_nstp[0], trans_nstp[1]]
+        )
+        reg.SetOptimizerScales(
+            [
+                1 / scal_init - 1 / (scal_init + scal_stp),
+                ang_stp,
+                trans_stp[0],
+                trans_stp[1],
+            ]
+        )
+    else:
+        reg.SetOptimizerAsGradientDescent(learningRate=lr, numberOfIterations=niter)
     # reg.SetOptimizerScalesFromPhysicalShift()
     param_dict = dict()
     reg.AddCommand(sitk.sitkIterationEvent, lambda: it_callback(reg, param_dict))
@@ -91,16 +99,21 @@ def est_sim(
     param_df = (
         pd.Series(param_dict)
         .reset_index(name="metric")
-        .rename(lambda c: re.sub("level_", "param_", c), axis="columns")
-    )
-    if verbose:
-        print("Scales: {}".format(param_df["param_0"].unique()))
-        print("Angles: {}".format(param_df["param_1"].unique()))
-        print("TransX: {}".format(param_df["param_2"].unique()))
-        print("TransY: {}".format(param_df["param_3"].unique()))
-        print(
-            "Scale: {}, Angle: {}, Trans: {}".format(
-                tx.GetScale(), tx.GetAngle(), tx.GetTranslation()
-            )
+        .rename(
+            columns={
+                "level_0": "scale",
+                "level_1": "angle",
+                "level_2": "transX",
+                "level_3": "transY",
+            }
         )
+    )
     return tx, param_df
+
+
+def estimate_tranform(src, dst, **kwargs):
+    tx_exh, param_exh = est_sim(src, dst, exhaustive=True, **kwargs)
+    tx_gd, param_gd = est_sim(src, dst, exhaustive=False, trans_init=tx_exh, **kwargs)
+    param_exh["stage"] = "exhaustive"
+    param_gd["stage"] = "gradient"
+    return tx_gd, tx_exh, pd.concat([param_exh, param_gd], ignore_index=True)
