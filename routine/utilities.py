4ec3451bb989a7cf5a3087a2753d5946a5316482
phildong <35715936+phildong@users.noreply.github.com>
2024-06-20 15:18:27 -0400

    update normalize and corr helper functions

--- routine/utilities.py
+++ routine/utilities.py
@@ -1,7 +1,15 @@
-def normalize(a):
-    amax, amin = a.max(), a.min()
+import numpy as np
+
+
+def normalize(a, q=(0, 1)):
+    amax, amin = np.nanquantile(a, q[1]), np.nanquantile(a, q[0])
     diff = amax - amin
     if diff > 0:
-        return (a - amin) / diff
+        return ((a - amin) / diff).clip(0, 1)
     else:
         return a
+
+
+def compute_corr(a, b):
+    a, b = np.nan_to_num(a).reshape(-1), np.nan_to_num(b).reshape(-1)
+    return np.corrcoef(a, b)[0, 1]
