9cec43075d7f573de96f91efa623dff68a9e96e7
phildong <35715936+phildong@users.noreply.github.com>
2025-02-18 17:54:45 +0000

    update spectrum extraction pipeline

--- 02.spectrum_extraction.py
+++ 02.spectrum_extraction.py
@@ -1,26 +1,23 @@
 # %% imports
 import os
-import pickle as pkl
-import warnings
 
 import cv2
 import holoviews as hv
+import pandas as pd
 import xarray as xr
+from tqdm.auto import tqdm
 
-from routine.coregistration import apply_tx
-from routine.io import load_dataset
+from routine.io import load_spectif
 from routine.plotting import plotA_contour
 from routine.utilities import normalize
 
 IN_DPATH = "./data/full/"
 IN_SS_CSV = "./data/full/sessions.csv"
-IN_REG_PATH = "./intermediate/co-registration/"
+IN_ROI_PATH = "./intermediate/cross-registration/rois/"
 OUT_PATH = "./intermediate/spectrum"
 FIG_PATH = "./figs/spectrum"
 PARAM_MED_WND = 3
 PARAM_SUMZ = False
-PARAM_TRANSFORM_ROI = True
-PARAM_FLIP_ROI = True
 PARAM_SKIP_EXISTING = False
 
 os.makedirs(OUT_PATH, exist_ok=True)
@@ -29,41 +26,22 @@ hv.extension("bokeh")
 
 
 # %% load data and extract spectrums
-for (anm, ss), ds, ssrow in load_dataset(IN_SS_CSV, IN_DPATH, flip_rois=PARAM_FLIP_ROI):
+ss_csv = pd.read_csv(IN_SS_CSV).set_index("animal")
+roi_files = list(filter(lambda f: f.endswith(".nc"), os.listdir(IN_ROI_PATH)))
+for r in tqdm(roi_files):
     # load data
-    dsname = "{}-{}".format(anm, ss)
-    if PARAM_SKIP_EXISTING and os.path.exists(
-        os.path.join(OUT_PATH, "{}.nc".format(dsname))
-    ):
-        continue
-    reg_ds = xr.open_dataset(os.path.join(IN_REG_PATH, "{}.nc".format(dsname)))
-    im_ms = reg_ds["ms-raw"].dropna("height", how="all").dropna("width", how="all")
-    im_conf = reg_ds["conf-raw"].dropna("height", how="all").dropna("width", how="all")
-    rois = ds["rois"]
-    ims_conf = ds["specs"]
-    # transform roi
-    if PARAM_TRANSFORM_ROI:
-        with open(
-            os.path.join(IN_REG_PATH, "tx-{}.pkl".format(dsname)), "rb"
-        ) as tx_file:
-            tx = pkl.load(tx_file)
-        rois = xr.apply_ufunc(
-            apply_tx,
-            rois,
-            input_core_dims=[["height", "width"]],
-            output_core_dims=[["height_new", "width_new"]],
-            vectorize=True,
-            kwargs={"tx": tx, "ref": im_conf},
-        )
-        rois = rois.rename(
-            {"height_new": "height", "width_new": "width"}
-        ).assign_coords(
-            {"height": im_conf.coords["height"], "width": im_conf.coords["width"]}
-        )
-    nempty = (rois.max(["height", "width"]) == 0).sum().item()
-    if nempty > 0:
-        warnings.warn("{} ROIs empty in dataset {}".format(nempty, dsname))
-    # load czi
+    rois = xr.open_dataset(os.path.join(IN_ROI_PATH, r))["rois"]
+    try:
+        anm, ss = rois.coords["animal"].item(), rois.coords["session"].item()
+        dsname = "{}-{}".format(anm, ss)
+    except KeyError:
+        anm = rois.coords["animal"].item()
+        dsname = anm
+    spec = ss_csv.loc[anm]["specs"].unique().item()
+    ims_conf = load_spectif(
+        os.path.dirname(os.path.join(IN_DPATH, spec)), os.path.basename(spec)
+    )
+    # process czi
     ims_conf = xr.apply_ufunc(
         cv2.medianBlur,
         ims_conf,
